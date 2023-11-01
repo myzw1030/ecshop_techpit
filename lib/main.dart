4bf2b46bdff4092ff2eb5010f70c6acfb786271c
miyazawa <13tmtmtommy@gmail.com>
2023-11-01 20:39:37 +0900

    デバイス固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:ecshop_techpit/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const App());
 }
