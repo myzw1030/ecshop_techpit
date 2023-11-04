24c365de60b930cbdf35a5ff05a5ffcbc9678f97
miyazawa <13tmtmtommy@gmail.com>
2023-11-04 23:27:48 +0900

    買い物かご

--- lib/pages/cart_page/cart_entry.dart
+++ lib/pages/cart_page/cart_entry.dart
@@ -1,10 +1,17 @@
+import 'package:ecshop_techpit/model/cart.dart';
+import 'package:ecshop_techpit/model/data/cart_item/cart_item.dart';
+import 'package:ecshop_techpit/model/shop_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartEntry extends StatelessWidget {
-  const CartEntry({super.key});
+class CartEntry extends ConsumerWidget {
+  const CartEntry({required this.cartItem, super.key});
+
+  final CartItem cartItem;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final itemInfo = ref.watch(shopItemForIdProvider(cartItem.itemId)).value!;
     return Padding(
       padding: const EdgeInsets.all(8),
       child: Row(
@@ -15,34 +22,41 @@ class CartEntry extends StatelessWidget {
               width: 100,
               height: 100,
               fit: BoxFit.cover,
-              'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1625&q=80',
+              itemInfo.imageUrl,
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Item Name',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleLarge
-                    ?.copyWith(fontWeight: FontWeight.bold),
-              ),
-              const Text('option - name'),
-              const Text(
-                '12,345 yen',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Row(
-                children: [
-                  const _ItemCounter(),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Delete'),
-                  ),
-                ],
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  itemInfo.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                Text('option - ${cartItem.option}'),
+                Text(
+                  '${itemInfo.price} yen',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    _ItemCounter(cartItem: cartItem),
+                    TextButton(
+                      onPressed: () {
+                        ref
+                            .read(cartProvider.notifier)
+                            .removeItem(cartItem.itemId, cartItem.option);
+                      },
+                      child: const Text('Delete'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
@@ -50,26 +64,38 @@ class CartEntry extends StatelessWidget {
   }
 }
 
-class _ItemCounter extends StatelessWidget {
-  const _ItemCounter();
+class _ItemCounter extends ConsumerWidget {
+  const _ItemCounter({required this.cartItem});
+
+  final CartItem cartItem;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       children: [
         SizedBox(
           width: 30,
           child: TextButton(
+            onPressed: cartItem.count > 1
+                ? () {
+                    ref
+                        .read(cartProvider.notifier)
+                        .decrementItemCount(cartItem.itemId, cartItem.option);
+                  }
+                : null,
             child: const Text('-'),
-            onPressed: () {},
           ),
         ),
-        const Text('12'),
+        Text('${cartItem.count}'),
         SizedBox(
           width: 30,
           child: TextButton(
             child: const Text('+'),
-            onPressed: () {},
+            onPressed: () {
+              ref
+                  .read(cartProvider.notifier)
+                  .incrementItemCount(cartItem.itemId, cartItem.option);
+            },
           ),
         )
       ],
--- lib/pages/cart_page/cart_page.dart
+++ lib/pages/cart_page/cart_page.dart
@@ -1,14 +1,17 @@
+import 'package:ecshop_techpit/model/cart.dart';
 import 'package:ecshop_techpit/pages/cart_page/cart_entry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends ConsumerWidget {
   const CartPage({super.key});
 
   static const path = 'cart';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cart = ref.watch(cartProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart'),
@@ -17,23 +20,41 @@ class CartPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: ((context, index) => const CartEntry()),
-              itemCount: 5,
+      body: ref.watch(cartItemCountProvider) > 0
+          ? Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: ((context, index) => CartEntry(
+                          cartItem: cart.elementAt(index),
+                        )),
+                    itemCount: ref.watch(cartItemCountProvider),
+                  ),
+                ),
+                ref.watch(cartItemCountProvider) > 0
+                    ? Padding(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                              '${ref.watch(totalPriceProvider)} yen - Buy'),
+                        ),
+                      )
+                    : Container(),
+              ],
+            )
+          : Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      'No Item',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 16),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Buy'),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
